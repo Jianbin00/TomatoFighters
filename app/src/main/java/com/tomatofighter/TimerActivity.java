938a65b356a71daccfe5ed395f57fe71c48a6fc9
Jianbin <lijianbin00@gmail.com>
2018-04-13 20:16:08 -0700

    Add TimerActivity and the layout files

--- app/src/main/java/com/tomatofighter/TimerActivity.java
+++ app/src/main/java/com/tomatofighter/TimerActivity.java
@@ -1,13 +1,15 @@
 package com.tomatofighter;
 
+import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.mcxtzhang.commonadapter.lvgv.CommonAdapter;
 import com.mcxtzhang.commonadapter.lvgv.ViewHolder;
@@ -16,16 +18,23 @@ import com.mcxtzhang.swipemenulib.SwipeMenuLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TodoEditorActivity extends AppCompatActivity
+
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+
+public class TimerActivity extends AppCompatActivity
 {
+
+    //FileOutputStream out;
     private ListView mLv;
     private List<TaskItem> mDatas;
     private Toolbar toolbar;
+    private TextView timer;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_editor);
+        setContentView(R.layout.activity_timer);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar()!=null)
@@ -33,9 +42,9 @@ public class TodoEditorActivity extends AppCompatActivity
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         mLv = findViewById(R.id.tasks);
-
+        timer=findViewById(R.id.remain_time);
         initDatas();
-        mLv.setAdapter(new CommonAdapter<TaskItem>(this, mDatas, R.layout.item_swipe_time_setter)
+        mLv.setAdapter(new CommonAdapter<TaskItem>(this, mDatas, R.layout.item_swipe_for_timer)
         {
             @Override
             public void convert(final ViewHolder holder, TaskItem taskItem, final int position)
@@ -56,10 +65,13 @@ public class TodoEditorActivity extends AppCompatActivity
                 });
             }
         });
+
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_todo_editor, menu);
+        getMenuInflater().inflate(R.menu.menu_timer, menu);
         return true;
     }
 
@@ -72,14 +84,9 @@ public class TodoEditorActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId())
         {
-            case R.id.action_play:
-                Intent i=new Intent(TodoEditorActivity.this,TimerActivity.class);
-                startActivity(i);
-                return true;
             case android.R.id.home:
                 finish();
                 return true;
-
         }
 
         return super.onOptionsItemSelected(item);
